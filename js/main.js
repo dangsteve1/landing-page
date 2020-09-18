750c6af06f1619600eb516fbe5d4933ddef05fa8
dangsteve1 <59983375+dangsteve1@users.noreply.github.com>
2020-09-17 21:04:06 -0700

    small edits

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-//DOM Elements
-
+// DOM Elements
 const time = document.getElementById("time"),
   greeting = document.getElementById("greeting"),
   name = document.getElementById("name"),
   focus = document.getElementById("focus");
 
-//Show Time
+// Options
+const showSeconds = false;
+const showAmPm = true;
+
+// Show Time
 function showTime() {
   let today = new Date(),
     hour = today.getHours(),
@@ -13,15 +16,16 @@ function showTime() {
     sec = today.getSeconds();
 
   // Set AM or PM
-  const amPM = hour >= 12 ? "PM" : "AM";
+  const amPm = hour >= 12 ? "PM" : "AM";
 
   // 12hr Format
   hour = hour % 12 || 12;
 
-  // Output time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
-    sec
-  )}`;
+  // Output Time
+  time.innerHTML = `
+  ${hour}<span>:</span>${addZero(min)}
+  ${showSeconds ? `<span>:</span> addZero(sec)` : ""}
+  ${showAmPm ? amPm : ""}`;
 
   setTimeout(showTime, 1000);
 }
@@ -49,7 +53,9 @@ function setBgGreet() {
   } else {
     // Night output
     document.body.style.backgroundImage =
+      // "url('https://i.ibb.co/924T2Wv/night.jpg')";
       "url('https://i.ibb.co/924T2Wv/night.jpg')";
+
     greeting.textContent = "Good Evening, ";
     document.body.style.color = "white";
   }
